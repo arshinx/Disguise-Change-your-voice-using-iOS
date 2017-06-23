680ec9fda1374b74321ac5402aaa6db2fab55906
Arshin Jain <arshin.jain@outlook.com>
2017-06-23 00:59:49 -0700

    Remove extraneous methods

--- Disguise/RecordSoundsViewController.swift
+++ Disguise/RecordSoundsViewController.swift
@@ -69,15 +69,4 @@ extension RecordSoundsViewController {
         super.viewDidLoad()
         stopButton.isEnabled = false
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
-    }
 }
