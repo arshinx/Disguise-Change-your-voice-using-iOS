eae8c434c5e36173c0378dafda5e3185be5777a5
Arshin Jain <arshin.jain@outlook.com>
2017-06-22 18:14:42 -0700

    Record Audio Action

--- Disguise/ViewController.swift
+++ Disguise/ViewController.swift
@@ -20,10 +20,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+    }
+    
 }
 
 // Actions
 extension ViewController {
+    
+    // Record
     
 }
