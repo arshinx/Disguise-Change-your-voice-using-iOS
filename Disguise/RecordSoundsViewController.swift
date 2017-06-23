ce082c38dd01e067c312eb49161998fc1ab8de8c
Arshin Jain <arshin.jain@outlook.com>
2017-06-23 00:28:07 -0700

    AVAudioRecorder var

--- Disguise/RecordSoundsViewController.swift
+++ Disguise/RecordSoundsViewController.swift
@@ -11,6 +11,9 @@ import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
     
+    // Variables
+    var audioRecorder: AVAudioRecorder!
+    
     // Outlets
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
@@ -29,6 +32,7 @@ class RecordSoundsViewController: UIViewController {
         recordingLabel.text = "Tap to Record"
         
         // Record Audio
+        
         
     }
     
