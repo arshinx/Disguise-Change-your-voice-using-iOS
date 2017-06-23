3b860c772f9dd9a5e0a9ec89f87dedea7ec01494
Arshin Jain <arshin.jain@outlook.com>
2017-06-23 00:56:31 -0700

    Add PlaySoundsVC

---
+++ Disguise/PlaySoundsTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySoundsTableViewController.swift
+//  Disguise
+//
+//  Created by Arshin Jain on 6/23/17.
+//  Copyright © 2017 Arshin Jain. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsTableViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
