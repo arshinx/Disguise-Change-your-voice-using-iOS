a36546674c52f483bb10014240c04e47a877ff0b
Arshin Jain <arshin.jain@outlook.com>
2017-06-22 18:15:53 -0700

    Refactoring and Comments

--- Disguise/ViewController.swift
+++ Disguise/ViewController.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    // Actions
     @IBAction func recordAudio(_ sender: Any) {
     }
     
 }
 
-// Actions
+// Manage ViewController
 extension ViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
