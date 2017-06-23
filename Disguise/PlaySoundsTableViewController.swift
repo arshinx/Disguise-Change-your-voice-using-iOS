3d15704e8f7fe51e4bc1ff3d02819b9b79c2b133
Arshin Jain <arshin.jain@outlook.com>
2017-06-23 00:56:48 -0700

    Correct Errors

--- Disguise/PlaySoundsTableViewController.swift
+++ Disguise/PlaySoundsTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PlaySoundsTableViewController: ViewController {
+class PlaySoundsTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
