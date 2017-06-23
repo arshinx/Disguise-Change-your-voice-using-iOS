a78a45860902f0d877c13fc56d23755f49c19cc5
Arshin Jain <arshin.jain@outlook.com>
2017-06-23 02:11:48 -0700

    Play Sounds VC

--- Disguise/PlaySoundsViewController.swift
+++ Disguise/PlaySoundsViewController.swift
@@ -13,6 +13,18 @@ class PlaySoundsViewController: UIViewController {
     // Variables
     var recordedAudioURL: URL?
     
+    // Outlets
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highpitchButton: UIButton!
+    @IBOutlet weak var lowpitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    // Actions
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
