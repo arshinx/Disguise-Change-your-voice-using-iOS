3689506617b57ec8664a0a6f68bd9b9c40b8b2e3
Arshin Jain <arshin.jain@outlook.com>
2017-06-22 18:16:09 -0700

    Remove extraneous comments

--- Disguise/ViewController.swift
+++ Disguise/ViewController.swift
@@ -21,11 +21,11 @@ extension ViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        .
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 }
